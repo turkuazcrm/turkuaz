285068ac346c8aa41a6ece597908de8914c10927
Berkhan Berkdemir <berkhan@tekdanisman.com>
2022-08-08 13:57:46 +0300

    Improve Turkish translation of Accounts module

--- pkg/vtiger/translations/TurkishLanguagePack_tr_tr/modules/Accounts.php
+++ pkg/vtiger/translations/TurkishLanguagePack_tr_tr/modules/Accounts.php
@@ -7,50 +7,53 @@
  * Portions created by vtiger are Copyright (C) vtiger.
  * All Rights Reserved.
  ************************************************************************************/
-$languageStrings = array(
-	'Accounts'                     => 'Müşteriler'                , 
-	'SINGLE_Accounts'              => 'Müşteri'                   , 
-	'LBL_ADD_RECORD'               => 'Add Organization'            , 
-	'LBL_RECORDS_LIST'             => 'Müşteri Listesi'           , 
-	'LBL_ACCOUNT_INFORMATION'      => 'Müşteri Bilgisi'           , 
-	'LBL_SHOW_ACCOUNT_HIERARCHY'   => 'Müşteri Hiyerarşisini göster', 
-	'industry'                     => 'Sektör'                     , 
-	'Account Name'                 => 'Müşteri Adı'              , 
-	'Account No'                   => 'Müşteri No'                , 
-	'Website'                      => 'Web Sayfası'                , 
-	'Ticker Symbol'                => 'Ticker Sembolü'             , 
-	'Member Of'                    => 'Ana Müşteri'               , 
-	'Employees'                    => 'Çalışanlar'               , 
-	'Ownership'                    => 'Ortaklığı'                , 
-	'SIC Code'                     => 'SIC Kodu'                    , 
-	'Other Email'                  => 'Diğer E-Posta Adresi'       , 
-	'Analyst'                      => 'Analist'                     , 
-	'Competitor'                   => 'Rakip'                       , 
-	'Customer'                     => 'Müşteri'                   , 
-	'Integrator'                   => 'Entegrator'                  , 
-	'Investor'                     => 'Yatırımcı'                , 
-	'Press'                        => 'Basın'                      , 
-	'Prospect'                     => 'Prospect'                    , 
-	'Reseller'                     => 'Reseller'                    , 
-        'Type'                         => 'Tip'                         , 
-	'LBL_START_DATE'               => 'Başlama Tarihi'             , 
-	'LBL_END_DATE'                 => 'Bitiş Tarihi'               , 
-	'LBL_DUPLICATES_EXIST'         => 'Organization Name already exists', 
-	'LBL_COPY_SHIPPING_ADDRESS'    => 'Kopya Teslimat Adresi'       , // TODO: Review
-	'LBL_COPY_BILLING_ADDRESS'     => 'Kopya Fatura Adresi'        , // TODO: Review
-    'LBL_IMAGE_INFORMATION' => 'Profil Resmi',
-    'Organization Image' => 'Organizasyon Görüntü',
+$languageStrings = [
+    'SINGLE_Accounts'            => 'Müşteri'                                  ,
 
-  'Other Phone' => 'İkincil Telefon',
-  'Phone' => 'Birincil Telefon',
-  'Email' => 'Birincil E-Posta',
+    'Account Name'               => 'Müşteri Adı'                              ,
+    'Account No'                 => 'Müşteri No'                               ,
+    'Accounts'                   => 'Müşteriler'                               ,
+    'Email'                      => 'Birincil E-Posta Adresi'                  ,
+    'Employees'                  => 'Çalışan Sayısı'                           ,
+    'industry'                   => 'Sektör'                                   ,
+    'Member Of'                  => 'Bağlı Olduğu Ana Müşteri'                 ,
+    'Organization Image'         => 'Organizasyon Görüntü'                     ,
+    'Other Email'                => 'İkincil E-Posta Adresi'                   ,
+    'Other Phone'                => 'İkincil Telefon'                          ,
+    'Ownership'                  => 'Aidiyeti'                                 , // TODO: Translation doesn't help
+    'Phone'                      => 'Birincil Telefon'                         ,
+    'SIC Code'                   => 'SIC Kodu'                                 ,
+    'Ticker Symbol'              => 'Stok Sembolü'                             ,
+    'Type'                       => 'Tip'                                      , // TODO: Translation doesn't help
+    'Website'                    => 'Web Sayfası'                              ,
 
-);
-$jsLanguageStrings = array(
-	'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Are you sure you want to Delete?', 
-	'LBL_DELETE_CONFIRMATION'      => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?', 
-	'LBL_MASS_DELETE_CONFIRMATION' => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?', 
+    'Analyst'                    => 'Analist'                                  ,
+    'Competitor'                 => 'Rakip'                                    ,
+    'Customer'                   => 'Müşteri'                                  ,
+    'Integrator'                 => 'Entegratör'                               ,
+    'Investor'                   => 'Yatırımcı'                                ,
+    'Partner'                    => 'Partner'                                  ,
+    'Press'                      => 'Basın'                                    ,
+    'Prospect'                   => 'Olası Müşteri'                            ,
+    'Reseller'                   => 'Satıcı'                                   ,
+    'Other'                      => 'Diğer'                                    ,
 
-  'JS_DUPLICATE_CREATION_CONFIRMATION' => 'Kuruluş Adı zaten Var.Yinelenen bir kayıt oluşturmak istiyor musunuz?',
+    'LBL_ACCOUNT_INFORMATION'    => 'Müşteri Bilgisi'                          ,
+    'LBL_ADD_RECORD'             => 'Müşteri Ekle'                             ,
+    'LBL_COPY_BILLING_ADDRESS'   => 'Fatura Teslimat Adresinden Kopyala'       ,
+    'LBL_COPY_SHIPPING_ADDRESS'  => 'Teslimat Adresinden Kopyala'              ,
+    'LBL_DUPLICATES_EXIST'       => 'Müşteri Zaten Var'                        ,
+    'LBL_END_DATE'               => 'Bitiş Tarihi'                             ,
+    'LBL_IMAGE_INFORMATION'      => 'Profil Resmi'                             ,
+    'LBL_RECORDS_LIST'           => 'Müşteri Listesi'                          ,
+    'LBL_SHOW_ACCOUNT_HIERARCHY' => 'Müşteri Hiyerarşisini Göster'             ,
+    'LBL_START_DATE'             => 'Başlama Tarihi'                           ,
+];
 
-);
+$jsLanguageStrings = [
+    'JS_DUPLICATE_CREATION_CONFIRMATION'     => 'Müşteri zaten var. Yinelenen bir kayıt oluşturmak istiyor musunuz?',
+
+    'LBL_DELETE_CONFIRMATION'                => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?',
+    'LBL_MASS_DELETE_CONFIRMATION'           => 'Deleting this Organization will remove its related Opportunities & Quotes. Are you sure you want to delete this Organization?',
+    'LBL_RELATED_RECORD_DELETE_CONFIRMATION' => 'Müşteri kaydını silmek istediğinizden emin misiniz?',
+];
