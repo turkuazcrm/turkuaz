20ac2a3c5a081d2cb9a9867ad652f78e6651c336
Berkhan Berkdemir <berkhan@tekdanisman.com>
2023-01-09 20:59:34 -0800

    Repackage modules with updated script

--- bin/archive-modules.sh
+++ bin/archive-modules.sh
@@ -48,70 +48,74 @@ cd ../../../../
 # Marketplace modules
 #
 cd pkg/vtiger/modules/ExtensionStore
-rm ../../../../packages/vtiger/mandatory/ExtensionStore.zip
+rm ../../../../packages/vtiger/marketplace/ExtensionStore.zip
 zip -9 -r ../../../../packages/vtiger/marketplace/ExtensionStore.zip *
 cd ../../../../
 
 # Optional Modules
 #
 cd pkg/vtiger/modules/Assets
-rm ../../../../packages/vtiger/mandatory/Assets.zip
+rm ../../../../packages/vtiger/optional/Assets.zip
 zip -9 -r ../../../../packages/vtiger/optional/Assets.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/CustomerPortal
-rm ../../../../packages/vtiger/mandatory/CustomerPortal.zip
+rm ../../../../packages/vtiger/optional/CustomerPortal.zip
 zip -9 -r ../../../../packages/vtiger/optional/CustomerPortal.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/EmailTemplates
-rm ../../../../packages/vtiger/mandatory/EmailTemplates.zip
+rm ../../../../packages/vtiger/optional/EmailTemplates.zip
 zip -9 -r ../../../../packages/vtiger/optional/EmailTemplates.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/Google
-rm ../../../../packages/vtiger/mandatory/Google.zip
+rm ../../../../packages/vtiger/optional/Google.zip
 zip -9 -r ../../../../packages/vtiger/optional/Google.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/ModComments
-rm ../../../../packages/vtiger/mandatory/ModComments.zip
+rm ../../../../packages/vtiger/optional/ModComments.zip
 zip -9 -r ../../../../packages/vtiger/optional/ModComments.zip *
 cd ../../../../
 
 # TODO: Each directory inside it needs to be archived as well
 #
 cd pkg/vtiger/modules/Projects
-rm ../../../../packages/vtiger/mandatory/Projects.zip
+rm ../../../../packages/vtiger/optional/Projects.zip
+mkdir tmp
 cd Project
-zip -9 -r ../Project.zip *
+zip -9 -r ../tmp/Project.zip *
 cd ../ProjectMilestone
-zip -9 -r ../ProjectMilestone.zip *
+zip -9 -r ../tmp//ProjectMilestone.zip *
 cd ../ProjectTask
-zip -9 -r ../ProjectTask.zip *
+zip -9 -r ../tmp/ProjectTask.zip *
 cd ../
-zip -9 -r ../../../../packages/vtiger/optional/Projects.zip *
-rm Project.zip ProjectMilestone.zip ProjectTask.zip
+cp manifest.xml tmp/
+cd tmp
+zip -9 -r ../../../../../packages/vtiger/optional/Projects.zip *
+cd ../
+rm -rf tmp
 cd ../../../../
 
 cd pkg/vtiger/modules/RecycleBin
-rm ../../../../packages/vtiger/mandatory/RecycleBin.zip
+rm ../../../../packages/vtiger/optional/RecycleBin.zip
 zip -9 -r ../../../../packages/vtiger/optional/RecycleBin.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/SMSNotifier
-rm ../../../../packages/vtiger/mandatory/SMSNotifier.zip
+rm ../../../../packages/vtiger/optional/SMSNotifier.zip
 zip -9 -r ../../../../packages/vtiger/optional/SMSNotifier.zip *
 cd ../../../../
 
 cd pkg/vtiger/modules/Webforms
-rm ../../../../packages/vtiger/mandatory/Webforms.zip
+rm ../../../../packages/vtiger/optional/Webforms.zip
 zip -9 -r ../../../../packages/vtiger/optional/Webforms.zip *
 cd ../../../../
 
 # Optional translation modules
 #
 cd pkg/vtiger/translations/TurkishLanguagePack_tr_tr
-rm ../../../../packages/vtiger/mandatory/TurkishLanguagePack_tr_tr.zip
+rm ../../../../packages/vtiger/optional/TurkishLanguagePack_tr_tr.zip
 zip -9 -r ../../../../packages/vtiger/optional/TurkishLanguagePack_tr_tr.zip *
 cd ../../../../
